3e065f73bb3b7edd0a7c3922cf64b1b4af957edc
praneethtkonda <praneethtkonda@gmail.com>
2015-03-05 18:14:38 +0530

    Sharing 1

--- hash.c
+++ hash.c
@@ -1,1 +1,16 @@
 #include<stdio.h>
+int main(){
+    int n,rev=0,i,rem,rem2;
+    printf("Enter the number to be displayed larger:");
+    scanf("%d",&n);
+    while(n!=0){
+	rem%=10;
+	rev=(rev*10)+rem;
+	n/=10;
+    }
+    while(rev!=0){
+	rem2%=10;
+	switch(rem2){
+	    case 
+    }
+}
